b7ab051c0006811e53eb7ccf82f49e29b9607307
Fernanda Gabrielli <123499117+FernandaGabrielli@users.noreply.github.com>
2025-04-28 19:46:24 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -4,28 +4,29 @@ FROM ItemPedido ip
 JOIN Produto p ON ip.produto_id = p.produto_id
 GROUP BY p.nome
 ORDER BY total_vendido DESC;
-
+ 
 -- 2. Clientes que mais compraram (último mês)
 SELECT u.nome, SUM(p.valor_total) AS total_gasto
 FROM Pedido p
 JOIN Usuario u ON p.usuario_id = u.usuario_id
-WHERE p.data_pedido >= DATEADD(month, -1, GETDATE())
+WHERE p.data_pedido >= DATE('now', '-1 month')
 GROUP BY u.nome
 ORDER BY total_gasto DESC;
-
--- 3. Estoque baixo (< 5 unidades)
+ 
+-- 3. Produtos com estoque baixo (< 5 unidades)
 SELECT nome, quantidade_estoque
 FROM Produto
 WHERE quantidade_estoque < 5;
-
--- 4. Faturamento por período (ex: último trimestre)
+ 
+-- 4. Faturamento por mês (últimos 3 meses)
 SELECT 
-    FORMAT(data_pedido, 'yyyy-MM') AS mes,
-    SUM(valor_total) AS faturamento
+  STRFTIME('%Y-%m', data_pedido) AS mes,
+  SUM(valor_total) AS faturamento
 FROM Pedido
-WHERE data_pedido >= DATEADD(month, -3, GETDATE())
-GROUP BY FORMAT(data_pedido, 'yyyy-MM');
-
+WHERE data_pedido >= DATE('now', '-3 months')
+GROUP BY STRFTIME('%Y-%m', data_pedido)
+ORDER BY mes;
+ 
 -- 5. Média de avaliações por produto
 SELECT p.nome, AVG(a.nota) AS media_avaliacoes
 FROM Produto p
